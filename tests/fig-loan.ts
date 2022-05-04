28da2b78cb89dd7ffe2d5736c048c54e582a9f0b
libraiger <misssilverbeauty0927@outlook.com>
2022-05-04 17:48:36 +0900

    Review Arweave

--- tests/fig-loan.ts
+++ tests/fig-loan.ts
@@ -7,20 +7,21 @@ import {
 } from './fig-loan_instructions';
 import fs from "fs";
 import Arweave from 'arweave';
-import { actions, utils, programs, NodeWallet, Connection} from '@metaplex/js'; 
-import { clusterApiUrl, Connection, Keypair, LAMPORTS_PER_SOL, Keypair, SystemProgram, PublicKey } from '@solana/web3.js';
-import { Metadata } from '@metaplex-foundation/mpl-token-metadata';
+import { actions, utils, programs, NodeWallet} from '@metaplex/js'; 
+import { clusterApiUrl, Connection, Keypair, LAMPORTS_PER_SOL, SystemProgram, PublicKey } from '@solana/web3.js';
+import { Metadata } from '@metaplex-foundation/mpl-token-metadata'; 
 
 describe('fig-loan', () => {
   const provider = anchor.Provider.env();
   anchor.setProvider(provider);
 
-  const program = anchor.workspace.FigLoan as Program<FigLoan>;
+//  const program = anchor.workspace.FigLoan as Program<FigLoan>;
 
   const admin = Keypair.generate();
   const user1 = Keypair.generate();
   const user2 = Keypair.generate();
-  let docUrl = undefined;
+  let videoUrl = undefined;
+  let metadata = undefined;
 
   (async () => {
 
@@ -29,54 +30,47 @@ describe('fig-loan', () => {
         host: 'arweave.net',
         port: 443,
         protocol: 'https',
-        timeout: 20000,
-        logging: false,
     });
+    const keypair = Keypair.generate();
+    console.log(keypair.publicKey.toBase58());
 
     // Upload image to Arweave
-    const data = fs.readFileSync('./1.docx');
-    const keypair = Keypair.generate();
-    
-    const transaction = await arweave.createTransaction({
-        data: data
-    });
-    
-    transaction.addTag('Content-Type', 'file/docx');
-    console.log("here");
-    
-    const wallet = await arweave.wallets.generate();
-    await arweave.transactions.sign(transaction, wallet);
-    
-    await arweave.transactions.post(transaction);
+    for(var i = 1;i <= 6; i++) {
+      console.log(i+"th Loop");
+      const fileData = fs.readFileSync(`./metadata/${i}.png`);
+      
+      const wallet = await arweave.wallets.generate();
+      const transaction = await arweave.createTransaction({
+          data: fileData
+      }, wallet);
+      
+      transaction.addTag('Content-Type', 'image/png');
+      
+      await arweave.transactions.sign(transaction, wallet);
+      
+      const uploader = await arweave.transactions.getUploader(transaction);
+      
+      while (!uploader.isComplete) {
+        await uploader.uploadChunk();
+        console.log(
+          `${uploader.pctComplete}% complete, ${uploader.uploadedChunks}/${uploader.totalChunks}`
+        );
+      }
 
-    const { id } = transaction;
-    docUrl = id ? `https://arweave.net/${id}` : undefined;
+      const { id } = transaction;
+      videoUrl = id ? `https://arweave.net/${id}` : undefined;
+      console.log(videoUrl);
 
-    const metadata = {
-      name: "Custom NFT #1",
-      symbol: "CNFT",
-      description:
-        "A description about my custom NFT #1",
-      seller_fee_basis_points: 500,
-      external_url: "https://www.customnft.com/",
-      attributes: [
-          {
-              trait_type: "NFT type",
-              value: "Custom"
-          }
-      ],
-      collection: {
-        name: "Test Collection",
-        family: "Custom NFTs",
-      },
-      properties: {
+      metadata = await require(`../metadata/${i}.json`);
+
+      metadata['properties'] = {
         files: [
           {
-            uri: docUrl,
-            type: "file/docx",
+            uri: videoUrl,
+            type: "video/mp4",
           },
         ],
-        category: "docx",
+        category: "mp4",
         maxSupply: 0,
         creators: [
           {
@@ -84,53 +78,51 @@ describe('fig-loan', () => {
             share: 100,
           },
         ],
-      },
-      image: docUrl,
+      };
+      metadata['video'] = videoUrl;
+      console.log(metadata);
+
+      const metadataRequest = JSON.stringify(metadata);
+      
+      const metadataTransaction = await arweave.createTransaction({
+          data: metadataRequest
+      });
+      
+      metadataTransaction.addTag('Content-Type', 'application/json');
+      
+      await arweave.transactions.sign(metadataTransaction, wallet);
+
+      const url = metadataTransaction.id;
+      videoUrl = url ? `https://arweave.net/${url}` : undefined;
+      
+      const response = await arweave.transactions.post(metadataTransaction);
+
+      const connection = new Connection(
+        clusterApiUrl('devnet'),
+        'confirmed',
+      );
+      const feePayerAirdropSignature = await connection.requestAirdrop(keypair.publicKey, LAMPORTS_PER_SOL);
+      await connection.confirmTransaction(feePayerAirdropSignature);
+      console.log("here");
+    
+      const mintNFTResponse = await actions.mintNFT({
+        connection,
+        wallet: new NodeWallet(keypair),
+        uri: videoUrl,
+        maxSupply: 1
+      });
+    
+      console.log(mintNFTResponse.mint.toBase58());
+      
+
+      /* Get URL from Address */
+
+      
+      const metadataPDA = await Metadata.getPDA(mintNFTResponse.mint);
+      await new Promise(f => setTimeout(f, 20000));
+      const tokenMetadata = await Metadata.load(connection, metadataPDA);
+      console.log(tokenMetadata.data.data.uri);
     }
-
-    const metadataRequest = JSON.stringify(metadata);
-    
-    const metadataTransaction = await arweave.createTransaction({
-        data: metadataRequest
-    });
-    
-    metadataTransaction.addTag('Content-Type', 'application/json');
-    
-    await arweave.transactions.sign(metadataTransaction, wallet);
-
-    const url = metadataTransaction.id;
-    docUrl = url ? `https://arweave.net/${url}` : undefined;
-    console.log("here");
-    
-    const response = await arweave.transactions.post(metadataTransaction);
-
-    const connection = new Connection(
-      clusterApiUrl('devnet'),
-      'confirmed',
-    );
-    const feePayerAirdropSignature = await connection.requestAirdrop(keypair.publicKey, LAMPORTS_PER_SOL);
-    await connection.confirmTransaction(feePayerAirdropSignature);
-    console.log("here");
-  
-    const mintNFTResponse = await actions.mintNFT({
-      connection,
-      wallet: new NodeWallet(keypair),
-      uri: docUrl,
-      maxSupply: 1
-    });
-  
-//    console.log(mintNFTResponse.mint.toBase58());
-    console.log(mintNFTResponse);
-    console.log(mintNFTResponse.mint.toBase58());
-    console.log(mintNFTResponse.metadata.toBase58());
-
-    /* Get URL from Address */
-
-    console.log("here");
-    const metadataPDA = await Metadata.getPDA(mintNFTResponse.metadata);
-    console.log("here");
-    const tokenMetadata = await Metadata.load(connection, metadataPDA);
-    console.log(tokenMetadata.data);
 
   })();
   
@@ -163,3 +155,16 @@ describe('fig-loan', () => {
   });
   */
 });
+
+// import { upload } from "./arweaveUpload";
+
+// const run = async () => {
+//   for (let i = 1; i <= 1; i++) {
+//     console.log(`Uploading ${i}...`);
+
+//     const pngUrl = await upload(`./metadata/${i}.png`);
+//     console.log(pngUrl);
+//   }
+// };
+
+// run();
